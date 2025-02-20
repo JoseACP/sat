ba8590ab4eddf1ddea82a0a092a72cc8c5cbaa8a
JoseACP <jose.chairez.is@unipolidgo.edu.mx>
2025-02-20 12:08:04 -0600

    sat

---
+++ MiProyectoCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿private static X509Certificate2 ObtenerKey(string thumbprint)
+{
+    X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+    store.Open(OpenFlags.ReadOnly);
+    var certificates = store.Certificates;
+    var certificatesEnc = certificates.Find(X509FindType.FindByThumbprint, thumbprint, false);
+    if (certificatesEnc.Count > 0)
+    {
+        X509Certificate2 certificate = certificatesEnc[0];
+        return certificate;
+    }
+    
+    return null;
+}
